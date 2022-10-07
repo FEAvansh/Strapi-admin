38f0af6b8cff46a03041bc320c9c3e9e19e37225
FEAvansh <vansh.frontendarmy@gmail.com>
2022-10-07 14:19:41 +0530

    "chnages"

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,32 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'HELLOWMYSELFADMINVANSH1316'),
+  },
+  auth: {
+    events: {
+      onConnectionSuccess(e) {
+        console.log(e.user, e.provider);
+      },
+      onConnectionError(e) {
+        console.error(e.error, e.provider);
+      },
+    },
+    options: {
+      expiresIn: '7d',
+    },
+    secret: env('ADMIN_JWT_SECRET', 'HELLOWTHISADMINSTRAPI1316'),
+  },
+  url: env('PUBLIC_ADMIN_URL', '/dashboard'),
+  autoOpen: false,
+  watchIgnoreFiles: [
+    './my-custom-folder', // Folder
+    './scripts/someScript.sh', // File
+  ],
+  host: 'localhost', // Only used for --watch-admin
+  port: 8003, // Only used for --watch-admin
+  serveAdminPanel: env.bool('SERVE_ADMIN', true),
+  forgotPassword: {
+    from: 'no-reply@example.com',
+    replyTo: 'no-reply@example.com',
   },
 });
