0c8c936b8638dff97920e6e2fb3b50725a83b70e
FEAvansh <vansh.frontendarmy@gmail.com>
2022-10-07 17:22:40 +0530

    "chnges"

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,15 @@
+// module.exports = ({ env }) => ({
+//   auth: {
+//     secret: env("ADMIN_JWT_SECRET"),
+//   },
+//   apiToken: {
+//     salt: env("API_TOKEN_SALT"),
+//   },
+// });
+
 module.exports = ({ env }) => ({
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'HELLOWMYSELFADMINVANSH1316'),
+    salt: env("API_TOKEN_SALT", "HELLOWMYSELFADMINVANSH1316"),
   },
   auth: {
     events: {
@@ -12,21 +21,21 @@ module.exports = ({ env }) => ({
       },
     },
     options: {
-      expiresIn: '7d',
+      expiresIn: "7d",
     },
-    secret: env('ADMIN_JWT_SECRET', 'HELLOWTHISADMINSTRAPI1316'),
+    secret: env("ADMIN_JWT_SECRET", "HELLOWTHISADMINSTRAPI1316"),
   },
-  url: env('PUBLIC_ADMIN_URL', '/dashboard'),
+  url: env("PUBLIC_ADMIN_URL", "/dashboard"),
   autoOpen: false,
   watchIgnoreFiles: [
-    './my-custom-folder', // Folder
-    './scripts/someScript.sh', // File
+    "./my-custom-folder", // Folder
+    "./scripts/someScript.sh", // File
   ],
-  host: 'localhost', // Only used for --watch-admin
+  host: "localhost", // Only used for --watch-admin
   port: 8003, // Only used for --watch-admin
-  serveAdminPanel: env.bool('SERVE_ADMIN', true),
+  serveAdminPanel: env.bool("SERVE_ADMIN", true),
   forgotPassword: {
-    from: 'no-reply@example.com',
-    replyTo: 'no-reply@example.com',
+    from: "no-reply@example.com",
+    replyTo: "no-reply@example.com",
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,3 +1,11 @@
+// module.exports = ({ env }) => ({
+//   host: env("HOST", "0.0.0.0"),
+//   proxy: true,
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+// });
+
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
   proxy: true,
