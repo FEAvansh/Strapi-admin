b2c5e2e23f38f31f8cc8f8c5c3da4b0b403f49c1
FEAvansh <vansh.frontendarmy@gmail.com>
2022-10-10 18:19:00 +0530

    "chnges"

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,35 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "mysql",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env("DATABASE_HOST", "auth-db162.hstgr.io"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "u401956558_reacttest"),
+      user: env("DATABASE_USERNAME", "u401956558_reacttest"),
+      password: env("DATABASE_PASSWORD", "reactTest@691"),
       ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
       },
     },
     debug: false,
   },
 });
+
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: config.host,
+//       port: config.port,
+//       database: config.database,
+//       user: config.user,
+//       password: config.password,
+//       ssl: {
+//         rejectUnauthorized: false
+//       },
+//     },
+//     debug: false,
+//   },
+// });
