e500164dd2bdbdf994f82930adebf22954d93ae0
FEAvansh <vansh.frontendarmy@gmail.com>
2022-10-14 15:11:12 +0530

    "changes main"

---
+++ src/api/naf/routes/naf.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * naf router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::naf.naf');
---
+++ src/api/naf/services/naf.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * naf service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::naf.naf');
