85335e5e773fa02324885ff3fe5cf55aad908880
FEAvansh <vansh.frontendarmy@gmail.com>
2022-10-14 19:11:33 +0530

    "chnages"

--- src/api/naf/controllers/naf.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * naf controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::naf.naf');
--- src/api/naf/routes/naf.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * naf router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::naf.naf');
